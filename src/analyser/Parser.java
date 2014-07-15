1b1610b42279a5b21757c5d158fe7934a530f771
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-07-15 21:57:06 +0100

    string parsing added

--- src/analyser/Parser.java
+++ src/analyser/Parser.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 
 class Parser
 {
+	protected final int IN_STRING = 0;
+	protected final int ESCAPED_CHAR = 1;
+	protected final int STRING_START = 2;
+	protected final int STRING_END = 3;
+
+	protected char currentStringDelimiter;
+	protected String currentString;
+	protected int currentStringStartIndex;
+
+	protected ArrayList<String> strings;
+
 	protected boolean parsing = false;
 	protected int currentCharIndex = -1;
 	protected int state = -1;
@@ -12,6 +23,7 @@ class Parser
 	{
 		this.parsing = true;
 		this.currentCharIndex = this.state = 0;
+		this.strings = new ArrayList<String>();
 	}
 
 	public void parseCodeChunk(String chunk)
@@ -40,6 +52,41 @@ class Parser
 
 	protected void parseChar(final char c)
 	{
+		if (c == '\\' && !this.compareState(this.ESCAPED_CHAR)) {
+			this.enableState(this.ESCAPED_CHAR);
+		}
+		else {
+			if ((c == '"' || c == '\'') && !this.compareState(this.IN_STRING)) {
+				this.currentStringDelimiter = c;
+				this.enableState(this.STRING_START);
+				this.currentStringStartIndex = this.currentCharIndex;
+				this.currentString = "";
+			}
+			else if (
+				(c == '"' || c == '\'') &&
+				this.compareState(this.IN_STRING) && !this.compareState(this.ESCAPED_CHAR)
+				&& c == this.currentStringDelimiter
+			) {
+				this.strings.add(this.currentString);
+				this.currentStringDelimiter = '\0';
+				this.enableState(this.STRING_END);
+				this.disableState(this.IN_STRING);
+			}
+
+			if (this.compareState(this.IN_STRING)) {
+				this.currentString = this.currentString.concat(String.valueOf(c));
+				if (this.compareState(this.ESCAPED_CHAR)) {
+					this.disableState(this.ESCAPED_CHAR);
+				}
+			}
+			else if (this.compareState(this.STRING_START)) {
+				this.disableState(this.STRING_START);
+				this.enableState(this.IN_STRING);
+			}
+			else if (this.compareState(this.STRING_END)) {
+				this.disableState(this.STRING_END);
+			}
+		}
 	}
 
 	public void endParsing()
