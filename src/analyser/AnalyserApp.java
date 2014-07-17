668fe643ed99623a3cc7acc874cadfacdaa43b2d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-07-17 08:03:23 +0100

    main class renamed

--- src/analyser/AnalyserApp.java
+++ src/analyser/AnalyserApp.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Analyser
+public class AnalyserApp
 {
 	public static void main(String[] argv)
 	{
