f90fef9869bda4debdb1e027b2a3b10365412435
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-07-19 22:46:20 +0100

    analysers doc added

--- src/jsanalyser/analyser/NumericAnalyser.java
+++ src/jsanalyser/analyser/NumericAnalyser.java
@@ -2,6 +2,25 @@ package jsanalyser.analyser;
 
 import java.util.Map;
 
+/**
+ * Analyser class for the numeric values.
+ * <p>
+ * A test will be executed on each numeric value to know if it is optimisable or
+ * not.
+ * </p>
+ * <p>
+ * A value is optimisable if, when stored in a variable, the resulting code is
+ * shorter than using the value each time.
+ * </p>
+ * <p>
+ * There are 2 different level of optimisation: if a new var keyword is needed
+ * or using a existing var.
+ * </p>
+ * <p>
+ * The fact that a value is optimisable (with or without a new var) depends on
+ * the value's size and on the number of occurences of the value.
+ * </p>
+ */
 public class NumericAnalyser extends Analyser
 {
 	public NumericAnalyser()
@@ -10,6 +29,10 @@ public class NumericAnalyser extends Analyser
 		this.reportTitle = "Numerical values:";
 	}
 
+	/**
+	 * The specific report will display if the code can be optimised by
+	 * fectorising the numeric value.
+	 */
 	public void specificReport(boolean verbose, String element, Integer nbOccurences)
 	{
 		int strLength = element.length(),
--- src/jsanalyser/analyser/RegexAnalyser.java
+++ src/jsanalyser/analyser/RegexAnalyser.java
@@ -2,6 +2,12 @@ package jsanalyser.analyser;
 
 import java.util.Map;
 
+/**
+ * Analyser class for the regexes.
+ * <p>
+ * For the moment, it has no specific report.
+ * </p>
+ */
 public class RegexAnalyser extends Analyser
 {
 	public RegexAnalyser()
